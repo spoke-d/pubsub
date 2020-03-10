24101c227405603cd12665cde49cc1ae58888813
Simon Richardson <stickupkid@gmail.com>
2020-03-10 15:00:32 +0000

    Remove concrete type for the subscriber.

--- multiplex.go
+++ multiplex.go
@@ -8,8 +8,16 @@ import (
 	"github.com/spoke-d/task"
 )
 
+// Sub defines a type that the multiplexer can run.
+type Sub interface {
+
+	// Run creates a task and a schedule to perform the consumption of messages
+	// sent to the subscriber from the origin.
+	Run(interval time.Duration) (task.Func, task.Schedule)
+}
+
 // Multiplexer forwards multiple subscribers to one singular hub.
-func Multiplexer(hub *Hub, subs ...*Subscriber) func(time.Duration) error {
+func Multiplexer(hub *Hub, subs ...Sub) func(time.Duration) error {
 	cleanups := make([]func(time.Duration) error, len(subs))
 	for i, sub := range subs {
 		cleanups[i], _ = task.Start(sub.Run(Interval))
